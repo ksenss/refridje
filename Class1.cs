31e2eb85dc41b899247336275a4ad18bb82f3669
dev <dev@A26PC3>
2024-05-24 11:40:58 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,160 @@
+﻿
+ 
+namespace refridje
+
+{
+
+    partial class Freezer
+
+    {
+
+
+        public string Model { set; get; } = "bosh";
+
+        public string Color { set; get; } = "pink";
+
+        private int price;
+
+        public int Price
+
+        {
+
+            get { return price; }
+
+            set
+
+            {
+
+                if (value > 0)
+
+                {
+
+                    price = value;
+
+                }
+
+                else
+
+                {
+
+                    price = 0;
+
+                }
+
+            }
+
+        }
+
+        private int height;
+
+        public int Height
+
+        {
+
+            get { return height; }
+
+            set
+
+            {
+
+                if (value > 0)
+
+                {
+
+                    height = value;
+
+                }
+
+                else
+
+                {
+
+                    height = 0;
+
+                }
+
+            }
+
+        }
+
+        private int width;
+
+        public int Width
+
+        {
+
+            get { return width; }
+
+            set
+
+            {
+
+                if (value > 0)
+
+                {
+
+                    width = value;
+
+                }
+
+                else
+
+                {
+
+                    width = 0;
+
+                }
+
+            }
+
+        }
+
+        private static string type;
+
+        private static string shop;
+
+        static Freezer()
+
+        {
+
+            shop = "kyivska 16";
+
+            type = "dry";
+
+        }
+
+
+
+    }
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Freezer[] freezers = new Freezer[5];
+
+            for (int i = 0; i < freezers.Length; i++)
+
+            {
+
+                freezers[i] = new Freezer(100 * (i + 1), 50 * (i + 1), 150 * (i + 1));
+
+            }
+
+            foreach (var freezer in freezers)
+
+            {
+
+                freezer.Print();
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refridje
+{
+    partial class Freezer
+    {
+        public Freezer()
+        {
+            price = 0;
+            width = 0;
+            height = 0;
+
+        }
+        public Freezer(int price, int width, int height)
+        {
+            Price = price;
+            Height = height;
+            width = Width;
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+        public override string ToString()
+        {
+            return $"Price: {Price}, Color: {Color}, Type: {type}, Shop: {shop}";
+        }
+
+    }
+}
